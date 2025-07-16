3c59feacea40adf2a4b4a2dd1452b0b7a623ecbf
Ajay Kumar Sahu <ajaykrdata@example.com>
2025-07-16 21:01:23 +0530

    Added initial project structure: Source, Data, Output

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+print("🚀 AI Data Clean Bot Starting...")
+
+# Load CSV
+df = pd.read_csv(r"C:\Users\Ajay\Desktop\AI_Data_Clean_bot\data\sample.csv")
+print("Loaded data:")
+print(df.head())
+
